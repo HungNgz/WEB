9720f4e0997fdcd465f6b06b96a6fc61e5142e59
HungNgz <114458794+HungNgz@users.noreply.github.com>
2023-05-16 16:02:43 +0700

    Add files via upload

---
+++ layout/footer.php
@@ -0,0 +1,24 @@
+
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
+<div class="container">
+    <hr />
+        <div class="text-center center-block">
+            <p class="txt-railway">- VINYLHANOI -</p>
+            <p class="txt-railway">- CONTACT WITH US -</p>
+            <br />
+                <a href="#"><i class="fa fa-facebook-square fa-3x social"></i></a>
+	            <a href="#"><i class="fa fa-twitter-square fa-3x social"></i></a>
+	            <a href="#"><i class="fa fa-google-plus-square fa-3x social"></i></a>
+	            <a href="#"><i class="fa fa-envelope-square fa-3x social"></i></a>
+</div>
+    <hr />
+</div>
+
+
+
+    <a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+</div>
+
+</body>
+</html>
